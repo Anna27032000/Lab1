036f69b1c63c56d2564dba414236c17d05d78f74
Anna27032000 <zv.annna00@gmail.com>
2020-09-20 09:32:00 +0300

    first commit

---
+++ src/main/java/org/zviaghintseva/untitled1/HelloController.java
@@ -0,0 +1,14 @@
+package org.zviaghintseva.untitled1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+@RequestMapping("/")
+    public String home()
+    {
+
+        return "home";
+    }
+}
